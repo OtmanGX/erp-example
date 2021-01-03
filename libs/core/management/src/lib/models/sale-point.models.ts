095cf9e814c74e0392cecdda1bc5e611d5cbe5e7
sh4im <chaymae.tamsamani@gmail.com>
2021-01-03 12:08:03 +0100

    [libs/core]starting core management

--- libs/core/management/src/lib/models/sale-point.models.ts
+++ libs/core/management/src/lib/models/sale-point.models.ts
@@ -1,2 +1,26 @@
-import { Companie } from "./companies.models";
+import {UsersProfile  } from "./use.models";
+
+export class SalePoint{
+    address:string;
+    email:string;
+    fax:string;
+    id:string;
+    name:string;
+    phone:string;
+  }
+
+export class DetailedSalePoint {
+    address:string;
+    email:string;
+    fax:string;
+    id:string;
+    name:string;
+    phone:string;
+    createdBy:string;
+    updatedBy:string;
+    usersProfiles:UsersProfile[];
+    createdAt:Date;
+    updatedAt:Date;
+  }
+
 
