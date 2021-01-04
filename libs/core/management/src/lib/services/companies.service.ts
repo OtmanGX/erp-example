edee7623f0c226983ba2346be3a9c31c0414c888
sh4im <chaymae.tamsamani@gmail.com>
2021-01-04 18:10:20 +0100

    core management services update

--- libs/core/management/src/lib/services/companies.service.ts
+++ libs/core/management/src/lib/services/companies.service.ts
@@ -1,26 +1,41 @@
+
 import { Injectable } from '@angular/core';
+import { useQuery,QueryResult } from '@apollo/client';
+import { Companie } from "../models/companies.models";
 import {
   GetAllCompaniesGQL,
   GetAllCompaniesQuery,
   GetAllCompaniesQueryVariables,
-
+  GetCompanyByIdGQL,
+  GetCompanyByIdQuery,
+  GetCompanyByIdQueryVariables,
+  InsertCompanyGQL,
+  InsertCompanyMutationVariables,InsertCompanyMutation,
+  Management_Company_Insert_Input,
+  GetAllCompaniesDocument
 } from '@tanglass-erp/infrastructure/graphql';
-import { ApolloQueryResult ,ApolloClientOptions,WatchQueryOptions,} from '@apollo/client/core';
-import {Observable  } from "rxjs";
+import { ApolloQueryResult ,ApolloClientOptions,WatchQueryOptions} from '@apollo/client/core';
+import {Observable,of  } from "rxjs";
+
 @Injectable({
   providedIn: 'root'
 })
 export class CompaniesService {
-
   constructor(
-    private getAllCompaniesGQL: GetAllCompaniesGQL,
+    private getAllGQL: GetAllCompaniesGQL,
+    private getByIdGQL:GetCompanyByIdGQL,
+    private insertOneGQL:InsertCompanyGQL,
 
   ) {}
 
 
-  getCompanies():Observable<ApolloQueryResult<GetAllCompaniesQuery>>{
-    return this.getAllCompaniesGQL.watch().valueChanges
+  getCompanies():Observable< QueryResult<Companie[], GetAllCompaniesQueryVariables>> {
+    return  of(useQuery<Companie[],GetAllCompaniesQueryVariables>(GetAllCompaniesDocument))
   }
 
+  getCompanyById(
+    $id?: GetCompanyByIdQueryVariables,):Observable<ApolloQueryResult<GetCompanyByIdQuery>>{
+    return  this.getByIdGQL.fetch($id)
+  }
 
 }
