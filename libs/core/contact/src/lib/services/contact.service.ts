cb0b43cc229925cab6bfc13113c5b6876429797f
sh4im <chaymae.tamsamani@gmail.com>
2021-01-14 17:28:07 +0100

    fixing issues

--- libs/core/contact/src/lib/services/contact.service.ts
+++ libs/core/contact/src/lib/services/contact.service.ts
@@ -27,11 +27,12 @@ export class ContactService {
     private updateOneGQL: UpdateContactGQL
 
   ) {
+    /** 
     this.getOneById('ee196a6a-9291-4932-b067-64d976cc5a20').subscribe(obj => { let data: DetailedContact = obj.data.contact_contact_by_pk; console.log(obj.data.contact_contact_by_pk) });
     this.getAll().subscribe(obj =>{let data: Contact[]=obj.data.contact_contact;console.log(obj.data.contact_contact)} );
     this.insertOne({phone:"phone",name:"name"}).subscribe(obj =>{let data: Contact=obj.data.insert_contact_contact_one;console.log(obj.data.insert_contact_contact_one)} );
     this.updateOne({id:"ee196a6a-9291-4932-b067-64d976cc5a20",phone:"phoneUpdate",name:"nameupdate"}).subscribe(obj =>{let data: Contact=obj.data.update_contact_contact_by_pk;console.log(obj.data.update_contact_contact_by_pk)} );
-
+*/
    }
 
   getAll() {
--- libs/core/contact/src/lib/services/customer.service.ts
+++ libs/core/contact/src/lib/services/customer.service.ts
@@ -25,12 +25,12 @@ export class CustomerService {
     private deleteOneGQL: DeleteCustomerGQL,
     private insertOneGQL: InsertCustomerGQL,
     private updateOneGQL: UpdateCustomerGQL) {
-
+/** 
     this.getOneById('ca6c4a90-dac3-46f0-945c-26ace051f52e').subscribe(obj => { let data: DetailedCustomer = obj.data.contact_customer_by_pk; console.log(obj.data.contact_customer_by_pk) });
     this.getAll().subscribe(obj => { let data: Customer[] = obj.data.contact_customer; console.log(obj.data.contact_customer) });
     this.insertOne({ addresses: [{ address: "address", city: "city" }], phone: "phone", name: "name" }).subscribe(obj => { let data: Customer = obj.data.insert_contact_customer_one; console.log(obj.data.insert_contact_customer_one) });
     this.updateOne({ id: "ca6c4a90-dac3-46f0-945c-26ace051f52e", phone: "phoneUpdate", name: "nameupdate" }).subscribe(obj => { let data: Customer = obj.data.update_contact_customer_by_pk; console.log(obj.data.update_contact_customer_by_pk) });
-
+*/
   }
 
   getAll() {
