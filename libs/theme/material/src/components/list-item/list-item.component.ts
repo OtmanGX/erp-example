775d2805a72a3355fb89644151638978e8745ecb
otmangx <otmangx@gmail.com>
2021-01-19 23:31:28 +0100

    Add fiche-card chips type

--- libs/theme/material/src/components/list-item/list-item.component.ts
+++ libs/theme/material/src/components/list-item/list-item.component.ts
@@ -1,11 +1,41 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component, ContentChild,
+  ContentChildren,
+  ElementRef,
+  Input,
+  OnInit,
+  QueryList, TemplateRef, ViewChild,
+  ViewContainerRef,
+  ViewRef
+} from '@angular/core';
+import { CardItem } from '../../interfaces/card-item';
 
 @Component({
   selector: 'ngx-list-item',
   templateUrl: './list-item.component.html',
   styleUrls: ['./list-item.component.scss']
 })
-export class ListItemComponent {
-  @Input() data: Array<any>;
+export class ListItemComponent implements OnInit, AfterViewInit, AfterContentInit {
+  @Input() data: Array<CardItem>;
   @Input() gap = "50px";
+  @ContentChild(TemplateRef) templateVariable: TemplateRef<any>;
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    // this.viewContainer.insert(this.allViews[0]);
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+  ngAfterContentInit(): void {
+    console.log(this.templateVariable);
+    console.log(this.templateVariable.elementRef);
+
+  }
+
 }
---
+++ libs/theme/material/src/interfaces/card-item.ts
@@ -0,0 +1,5 @@
+export interface CardItem {
+  label: string;
+  value?: any;
+  type?: string;
+}
