1c05d742162d4b4b090d8ece003081b503f607d1
otmangx <otmangx@gmail.com>
2021-06-19 00:55:55 +0100

    [sales] add invoice_lines and fix issues

--- libs/store/sales/src/lib/+state/invoice/invoice.effects.ts
+++ libs/store/sales/src/lib/+state/invoice/invoice.effects.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@angular/core';
 import { createEffect, Actions, ofType } from '@ngrx/effects';
 import * as InvoiceActions from './invoice.actions';
 import { catchError, map, mergeMap } from 'rxjs/operators';
-import { InsertedInvoice, InvoiceService, UpdatedInvoice } from '@tanglass-erp/core/sales';
+import {
+  InsertedInvoice,
+  InvoiceService,
+  UpdatedInvoice,
+} from '@tanglass-erp/core/sales';
 import { NotificationFacadeService } from '@tanglass-erp/store/app';
 import { of } from 'rxjs';
 import { Router } from '@angular/router';
@@ -11,109 +15,156 @@ import { Router } from '@angular/router';
 export class InvoiceEffects {
   loadinvoices$ = createEffect(() =>
     this.actions$.pipe(
-    ofType(InvoiceActions.loadInvoices),
-    mergeMap(() => this.service.getAll()
-      .pipe(
-        map(data => InvoiceActions.loadInvoicesSuccess({invoices: data.data.sales_invoice})),
-        catchError(error => {
-          this.notificationService.showToast('error', 'Erreur de chargement', error);
-          return of(InvoiceActions.loadInvoicesFailure({error}));
-        })
-      ))
+      ofType(InvoiceActions.loadInvoices),
+      mergeMap(() =>
+        this.service.getAll().pipe(
+          map((data) =>
+            InvoiceActions.loadInvoicesSuccess({
+              invoices: data.data.sales_invoice,
+            })
+          ),
+          catchError((error) => {
+            this.notificationService.showToast(
+              'error',
+              'Erreur de chargement',
+              error
+            );
+            return of(InvoiceActions.loadInvoicesFailure({ error }));
+          })
+        )
+      )
     )
   );
 
   loadinvoiceById$ = createEffect(() =>
     this.actions$.pipe(
-    ofType(InvoiceActions.loadInvoiceById),
-    mergeMap(action => this.service.getOneById(action.id)
-      .pipe(
-        map(data => {
-          const {__typename, ...invoice} = data.data.sales_invoice_by_pk;
-          return InvoiceActions.loadInvoiceByIdSuccess({invoice: <UpdatedInvoice> invoice})
-        }),
-        catchError(error => {
-          this.notificationService.showToast('error', 'Erreur de chargement', error);
-          return of(InvoiceActions.loadInvoiceByIdFailure({error}));
-        })
-      ))
+      ofType(InvoiceActions.loadInvoiceById),
+      mergeMap((action) =>
+        this.service.getOneById(action.id).pipe(
+          map((data) => {
+            const { __typename, ...invoice } = data.data.sales_invoice_by_pk;
+            return InvoiceActions.loadInvoiceByIdSuccess({
+              invoice: <UpdatedInvoice>invoice,
+            });
+          }),
+          catchError((error) => {
+            this.notificationService.showToast(
+              'error',
+              'Erreur de chargement',
+              error
+            );
+            return of(InvoiceActions.loadInvoiceByIdFailure({ error }));
+          })
+        )
+      )
     )
   );
 
   addInvoice$ = createEffect(() =>
     this.actions$.pipe(
-    ofType(InvoiceActions.addInvoice),
-    mergeMap(action => this.service.insertOne(action.invoice)
-      .pipe(
-        map(data => {
-          this.notificationService.showNotifToast({
-            message: 'Ajouté avec succès',
-            operation: 'success',
-            title: 'Factures',
-            time: new Date(),
-            icon: 'recipt',
-            route: 'sales/invoice',
-            color: 'primary'
-          })
-          this.router.navigate(['sales/invoice/ready'], {state: {data: action.invoice}});
-          return InvoiceActions.addInvoiceSuccess({invoice: data.data.insert_sales_invoice_one})
-        }),
-        catchError(error => {
-          this.notificationService.showToast('error', 'Erreur d\'ajout', error);
-          return of(InvoiceActions.addInvoiceFailure({error}));
-        })
-      ))
+      ofType(InvoiceActions.addInvoice),
+      mergeMap((action) => {
+        return this.service
+          .prepareInvoiceLines(action.invoice.deliveries.map((e) => e.delivery_id))
+          .pipe(mergeMap((value) => {
+            const invoice = {
+              ...action.invoice,
+              invoice_lines: value
+            };
+            return this.service.insertOne(invoice)
+          }
+          ))
+          .pipe(
+            map((data) => {
+              this.notificationService.showNotifToast({
+                message: 'Ajouté avec succès',
+                operation: 'success',
+                title: 'Factures',
+                time: new Date(),
+                icon: 'recipt',
+                route: 'sales/invoice',
+                color: 'primary',
+              });
+              this.router.navigate(['sales/invoice/ready'], {
+                state: { data: data.data.insert_sales_invoice_one },
+              });
+              return InvoiceActions.addInvoiceSuccess({
+                invoice: data.data.insert_sales_invoice_one,
+              });
+            }),
+            catchError((error) => {
+              this.notificationService.showToast(
+                'error',
+                "Erreur d'ajout",
+                error
+              );
+              return of(InvoiceActions.addInvoiceFailure({ error }));
+            })
+          );
+      })
     )
   );
 
   updateInvoice$ = createEffect(() =>
     this.actions$.pipe(
-    ofType(InvoiceActions.updateInvoice),
-    mergeMap(action => this.service.updateOne(action.invoice)
-      .pipe(
-        map(data => {
-          this.router.navigate(['sales/invoice']);
-          this.notificationService.showNotifToast({
-            message: 'Mise à jour avec succès',
-            operation: 'info',
-            title: 'Factures',
-            time: new Date(),
-            icon: 'checked',
-            route: 'sales/invoice',
-            color: 'accent'
+      ofType(InvoiceActions.updateInvoice),
+      mergeMap((action) =>
+        this.service.updateOne(action.invoice).pipe(
+          map((data) => {
+            this.router.navigate(['sales/invoice']);
+            this.notificationService.showNotifToast({
+              message: 'Mise à jour avec succès',
+              operation: 'info',
+              title: 'Factures',
+              time: new Date(),
+              icon: 'checked',
+              route: 'sales/invoice',
+              color: 'accent',
+            });
+            return InvoiceActions.updateInvoiceSuccess({
+              invoice: data.data.update_sales_invoice_by_pk,
+            });
+          }),
+          catchError((error) => {
+            this.notificationService.showToast(
+              'error',
+              'Erreur de mise à jour',
+              error
+            );
+            return of(InvoiceActions.updateInvoiceFailure({ error }));
           })
-          return InvoiceActions.updateInvoiceSuccess({invoice: data.data.update_sales_invoice_by_pk})
-        }),
-        catchError(error => {
-          this.notificationService.showToast('error', 'Erreur de mise à jour', error);
-          return of(InvoiceActions.updateInvoiceFailure({error}));
-        })
-      ))
+        )
+      )
     )
   );
 
   removeInvoice$ = createEffect(() =>
     this.actions$.pipe(
-    ofType(InvoiceActions.deleteInvoices),
-    mergeMap(action => this.service.deleteMany(action.ids)
-      .pipe(
-        map(data => {
-          this.notificationService.showNotifToast({
-            message: 'Supprimé avec succès',
-            operation: 'info',
-            title: 'Factures',
-            time: new Date(),
-            icon: 'close',
-            route: 'sales/invoice',
-            color: 'warn'
+      ofType(InvoiceActions.deleteInvoices),
+      mergeMap((action) =>
+        this.service.deleteMany(action.ids).pipe(
+          map((data) => {
+            this.notificationService.showNotifToast({
+              message: 'Supprimé avec succès',
+              operation: 'info',
+              title: 'Factures',
+              time: new Date(),
+              icon: 'close',
+              route: 'sales/invoice',
+              color: 'warn',
+            });
+            return InvoiceActions.deleteInvoicesSuccess({ ids: action.ids });
+          }),
+          catchError((error) => {
+            this.notificationService.showToast(
+              'error',
+              'Erreur de suppression',
+              error
+            );
+            return of(InvoiceActions.deleteInvoicesFailure({ error }));
           })
-          return InvoiceActions.deleteInvoicesSuccess({ids: action.ids})
-        }),
-        catchError(error => {
-          this.notificationService.showToast('error', 'Erreur de suppression', error);
-          return of(InvoiceActions.deleteInvoicesFailure({error}));
-        })
-      ))
+        )
+      )
     )
   );
 
@@ -121,6 +172,6 @@ export class InvoiceEffects {
     private actions$: Actions,
     private service: InvoiceService,
     private router: Router,
-    private notificationService: NotificationFacadeService,
+    private notificationService: NotificationFacadeService
   ) {}
 }
--- libs/store/sales/src/lib/+state/invoice/invoice.facade.ts
+++ libs/store/sales/src/lib/+state/invoice/invoice.facade.ts
@@ -7,6 +7,7 @@ import * as InvoiceSelectors from './invoice.selectors';
 import * as InvoiceActions from './invoice.actions';
 import { InsertedInvoice, UpdatedInvoice } from '@tanglass-erp/core/sales';
 import { filter, switchMap } from 'rxjs/operators';
+import { DeliveryFacade } from '../delivery/delivery.facade';
 
 @Injectable()
 export class InvoiceFacade {
@@ -20,7 +21,10 @@ export class InvoiceFacade {
     filter((e) => !!e)
   );
 
-  constructor(private store: Store<fromInvoice.InvoicePartialState>) {}
+  constructor(
+    private store: Store<fromInvoice.InvoicePartialState>,
+    private deliveryFacade: DeliveryFacade
+    ) {}
 
   dispatch(action: Action) {
     this.store.dispatch(action);
