e0013dca0676fc1e43bd4561c372428fbec603c3
BENAYAD OTMANE <otmangx@gmail.com>
2021-10-25 18:57:04 +0100

    add service card actions

--- apps/ui/product/src/app/components/service-card-item/service-card-item.component.ts
+++ apps/ui/product/src/app/components/service-card-item/service-card-item.component.ts
@@ -1,12 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ngx-service-card-item',
   templateUrl: './service-card-item.component.html',
   styleUrls: ['./service-card-item.component.scss']
 })
-export class ServiceCardItemComponent {
+export class ServiceCardItemComponent implements AfterViewInit {
   @Input() data;
+  @Input() showActions = false;
   constructor() {
+  }
+
+  ngAfterViewInit(): void {
   }
 }
--- apps/ui/product/src/app/pages/list-service/list-service.component.ts
+++ apps/ui/product/src/app/pages/list-service/list-service.component.ts
@@ -14,6 +14,7 @@ import { Store } from '@ngrx/store';
 export class ListServiceComponent implements OnInit {
 
   data$ = this.store.select(ServiceGroupeSelectors.getAllServiceConfigs);
+  viewActions: boolean = false;
 
   constructor(private dialog: MatDialog,
     private store: Store
